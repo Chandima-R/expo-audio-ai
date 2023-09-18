957c158590133de2216a0522e13e542114f49766
Chandima <chandimar.online@gmail.com>
2023-09-19 00:01:34 +0530

    complete frontend

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,195 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Audio } from 'expo-av';
+import {useRoute} from "@react-navigation/native";
+import {widthPercentageToDP as wp} from "react-native-responsive-screen";
+import {Image} from "expo-image";
+
+export default function HomeScreen() {
+    const [recording, setRecording] = React.useState();
+    const [recordings, setRecordings] = React.useState([]);
+    const [message, setMessage] = React.useState("");
+    const [playingSound, setPlayingSound] = React.useState(null);
+    const [playingSoundIndex, setPlayingSoundIndex] = React.useState(null);
+
+
+    const route = useRoute()
+    const {selectedLanguage, selectedGrade} = route.params
+
+    async function startRecording() {
+        try {
+            const permission = await Audio.requestPermissionsAsync();
+
+            if (permission.status === "granted") {
+                await Audio.setAudioModeAsync({
+                    allowsRecordingIOS: true,
+                    playsInSilentModeIOS: true
+                });
+
+                const { recording } = await Audio.Recording.createAsync(
+                    Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+                );
+
+                setRecording(recording);
+            } else {
+                setMessage("Please grant permission to app to access microphone");
+            }
+        } catch (err) {
+            console.error('Failed to start recording', err);
+        }
+    }
+
+    async function stopRecording() {
+        setRecording(undefined);
+        await recording.stopAndUnloadAsync();
+
+        let updatedRecordings = [...recordings];
+        const { sound, status } = await recording.createNewLoadedSoundAsync();
+        updatedRecordings.push({
+            sound: sound,
+            duration: getDurationFormatted(status.durationMillis),
+            file: recording.getURI()
+        });
+
+        setRecordings(updatedRecordings);
+    }
+
+    function getDurationFormatted(millis) {
+        const minutes = millis / 1000 / 60;
+        const minutesDisplay = Math.floor(minutes);
+        const seconds = Math.round((minutes - minutesDisplay) * 60);
+        const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+        return `${minutesDisplay}:${secondsDisplay}`;
+    }
+
+    function getRecordingLines() {
+        return recordings.map((recordingLine, index) => {
+            const isPlaying = playingSoundIndex === index;
+            return (
+                <View key={index} style={styles.row}>
+                    <Text style={styles.fill}>
+                        Recording {index + 1} - {recordingLine.duration}
+                    </Text>
+                    <TouchableOpacity
+                        style={styles.messageListButton}
+                        onPress={() => {
+                            if (isPlaying) {
+                                // If the recording is already playing, stop it
+                                playingSound.stopAsync();
+                                setPlayingSound(null);
+                                setPlayingSoundIndex(null);
+                            } else {
+                                // If it's not playing, start playing it
+                                recordingLine.sound.replayAsync();
+                                setPlayingSound(recordingLine.sound);
+                                setPlayingSoundIndex(index);
+                            }
+                        }}
+                    >
+                        <Text style={styles.messageListButtonText}>
+                            {isPlaying ? "Stop" : "Play"}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        });
+    }
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image contentFit="cover"
+                   transition={1000}
+                   source="https://picsum.photos/seed/696/3000/2000"
+                   style={styles.logoImage}
+                />
+                <View>
+                    <Text style={styles.languageText}>Language: {selectedLanguage}</Text>
+                    <Text style={styles.gradeText}>Grade: {selectedGrade}</Text>
+                </View>
+            </View>
+
+            <ScrollView style={styles.messageList}>
+                <Text style={styles.messageListTitle}>Responses</Text>
+                <Text>{message}</Text>
+                {getRecordingLines()}
+            </ScrollView>
+
+            <TouchableOpacity
+                style={styles.recordingButton}
+                onPress={recording ? stopRecording : startRecording}>
+                <Text style={styles.buttonText}>{recording ? 'Stop Recording' : 'Start Recording'}</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 60,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    recordingButton:{
+        backgroundColor: '#059669',
+        zIndex: 50,
+        padding: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    buttonText:{
+        fontSize: wp(4),
+        color: '#fff',
+        textAlign: 'center',
+    },
+    imageContainer:{
+        display: 'flex',
+        alignItems: "center",
+    },
+    logoImage:{
+        marginRight: 20,
+        width: '100%',
+        backgroundColor: '#0553',
+    },
+    languageText:{
+        fontSize: wp(4),
+    },
+    gradeText:{
+        fontSize: wp(4),
+    },
+    messageList:{
+        backgroundColor: '#fafafa',
+        flex: 1,
+        width: '100%',
+        padding: 10,
+        marginTop: 10
+    },
+    messageListTitle:{
+        fontSize: wp(4),
+        textTransform: 'capitalize'
+    },
+    row:{
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
+    messageListButton:{
+
+        backgroundColor: '#34D399',
+        padding:10,
+        borderRadius: 5,
+    },
+    messageListButtonText:{
+        fontSize: wp(4),
+        color: '#fff',
+    }
+});
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,129 @@
+import {SafeAreaView, Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import {Picker} from "@react-native-picker/picker";
+import {useState} from "react";
+import {
+    widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import { Image } from 'expo-image';
+import {useNavigation} from '@react-navigation/native';
+import {StatusBar} from "expo-status-bar";
+
+
+export default function WelcomeScreen(){
+    const [selectedLanguage, setSelectedLanguage] = useState('English');
+    const [selectedGrade, setSelectedGrade] = useState('1');
+    const navigation = useNavigation();
+    return(
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.textContainer}>
+                <Text
+                    style={styles.titleText}>
+                    Audio AI
+                </Text>
+                <Text
+                    style={styles.detailsText}>
+                    Future is here, powered by AI.
+                </Text>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image
+                    source='../../assets/images/welcome.png'
+                    style={{width: wp(75), height: wp(75)}}
+                    contentFit="cover"
+                    transition={1000}
+                />
+            </View>
+            <View>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={selectedLanguage}
+                        onValueChange={itemValue => setSelectedLanguage(itemValue)}>
+                        <Picker.Item label="English" value="English" />
+                        <Picker.Item label="Sinhala" value="Sinhala" />
+                        <Picker.Item label="Tamil" value="Tamil" />
+                    </Picker>
+                </View>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={selectedGrade}
+                        onValueChange={itemValue => setSelectedGrade(itemValue)}>
+                        {Array.from({length: 13}, (_, i) => (
+                            <Picker.Item
+                                key={i.toString()}
+                                label={`${i + 1}`}
+                                value={`${i + 1}`}
+                            />
+                        ))}
+                    </Picker>
+                </View>
+            </View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() =>
+                    navigation.navigate('Home', {selectedGrade, selectedLanguage})
+                }>
+                <Text
+                    style={styles.buttonText}
+                >
+                    Get Started
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        display: "flex",
+        flex: 1,
+        justifyContent: 'space-around',
+        backgroundColor: 'white',
+        padding: 20
+    },
+    textContainer:{
+        marginTop: "0.5rem",
+    },
+    titleText:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color :'#3F3F46',
+        fontSize: wp(10)
+    },
+    detailsText:{
+        fontSize: wp(4),
+        color:'#52525B',
+        fontWeight: '600',
+        textAlign: 'center',
+    },
+    imageContainer:{
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    pickerContainer: {
+        borderWidth: 1,
+        borderColor: '##34D399',
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    picker: {
+        height: 40,
+        width: '100%',
+        paddingLeft: 10,
+    },
+    button:{
+        backgroundColor: '#059669',
+        zIndex: 50,
+        padding: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    buttonText:{
+        fontSize: wp(6),
+        color: '#fff',
+        textAlign: 'center',
+    }
+})
