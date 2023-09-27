802436fc645ee8198eaf41ad1d74b6b2abccbbab
chandima <chandimar.online@gmail.com>
2023-09-27 23:25:11 +0530

    fix request issue

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,8 +25,10 @@ export default function HomeScreen() {
   const [recordedAudioData, setRecordedAudioData] = useState(null);
   const [downloadedAudioUri, setDownloadedAudioUri] = useState(null);
   const [isPlayingDownloadedAudio, setIsPlayingDownloadedAudio] = useState(
-    false
+      false
   );
+  const [downloadedSound, setDownloadedSound] = useState(null); // Added
+  const [isLoading, setIsLoading] = useState(false); // Added
 
   const route = useRoute();
   const { selectedLanguage, selectedGrade } = route.params;
@@ -60,10 +62,40 @@ export default function HomeScreen() {
     }
   };
 
+  const handleMP3Response = async (mp3Url) => {
+    if (mp3Url) {
+      try {
+        // Download the MP3 file
+        const { uri } = await FileSystem.downloadAsync(
+            mp3Url,
+            FileSystem.cacheDirectory + 'audio.mp3'
+        );
+
+        setDownloadedAudioUri(uri);
+
+        // Create and load the sound object
+        const soundObject = new Audio.Sound();
+        await soundObject.loadAsync({ uri });
+        setDownloadedSound(soundObject);
+
+        // Play the downloaded audio
+        await soundObject.playAsync();
+        soundObject.setOnPlaybackStatusUpdate((status) => {
+          if (!status.isPlaying) {
+            setIsPlayingDownloadedAudio(false);
+          }
+        });
+
+        setIsPlayingDownloadedAudio(true);
+      } catch (error) {
+        console.error('Failed to handle MP3 response:', error);
+      }
+    }
+  };
   async function sendAudioToAPI(audioData, selectedLanguage, selectedGrade) {
     try {
-      const apiUrl = 'http://192.168.8.133:5000/chatbot';
-  
+      const apiUrl = 'https://536d-2402-4000-b281-f462-25ff-529d-118a-258b.ngrok-free.app/chatbot';
+
       const formData = new FormData();
       formData.append('language', selectedLanguage);
       formData.append('grade', selectedGrade);
@@ -72,53 +104,61 @@ export default function HomeScreen() {
         name: 'audio.wav',
         type: 'audio/wav',
       });
-  
+
       console.log('Sending audio data:', audioData);
-  
+
+      setIsLoading(true); // Enable loading indicator
+
       const response = await axios.post(apiUrl, formData, {
         headers: {
-            'Content-Type': 'multipart/form-data',
-            'Accept': 'audio/mpeg', 
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'audio/mpeg',
         },
       });
-  
-      console.log('API response:', response.data);
-  
-      if (response.data.audioUrl) {
-       
-        const { uri } = await FileSystem.downloadAsync(
-          response.data.audioUrl,
-          FileSystem.cacheDirectory + 'audio.mp3'
-        );
+      setIsLoading(false); // Disable loading indicator
 
-        console.log(58)
-  
-        setDownloadedAudioUri(uri);
+      console.log(77)
+      console.log(854, 'API response:', response?.data);
+
+      console.log(88)
+      if (response.data) {
+        // Check if the response contains an audio URL
+        if (response.data.audioUrl) {
+          // Call handleMP3Response to handle the MP3 audio playback
+          await handleMP3Response(response.data.audioUrl);
+        } else {
+          console.error('API response does not contain audio URL');
+        }
       } else {
         console.error('API response does not contain audio URL');
       }
     } catch (error) {
       console.error('Failed to send audio to API:', error);
+      setIsLoading(false); // Disable loading indicator on error
     }
   }
-  
+
   const startRecording = async () => {
     try {
       if (recording) {
+        setIsLoading(true); // Enable loading indicator
         await recording.startAsync();
         setIsRecording(true);
+        setIsLoading(false); // Disable loading indicator
       } else {
         setMessage(
-          'Recording object is not available. Please wait for initialization to complete.'
+            'Recording object is not available. Please wait for initialization to complete.'
         );
       }
     } catch (error) {
       console.error('Failed to start recording:', error);
+      setIsLoading(false); // Disable loading indicator on error
     }
   };
 
   const stopRecording = async () => {
     if (recording) {
+      setIsLoading(true); // Enable loading indicator
       setIsRecording(false);
       try {
         await recording.stopAndUnloadAsync();
@@ -137,34 +177,28 @@ export default function HomeScreen() {
         await sendAudioToAPI(audioData, selectedLanguage, selectedGrade);
       } catch (error) {
         console.error('Failed to stop recording:', error);
+      } finally {
+        setIsLoading(false); // Disable loading indicator
       }
     }
   };
 
-  const toggleDownloadedAudioPlayback = () => {
+  const toggleDownloadedAudioPlayback = async () => {
     if (isPlayingDownloadedAudio) {
-      playingSound.stopAsync();
-      setPlayingSound(null);
+      downloadedSound.stopAsync();
       setIsPlayingDownloadedAudio(false);
     } else if (downloadedAudioUri) {
-      const soundObject = new Audio.Sound();
-      soundObject
-        .loadAsync({ uri: downloadedAudioUri })
-        .then(() => {
-          soundObject.playAsync();
-          soundObject.setOnPlaybackStatusUpdate((status) => {
-            if (!status.isPlaying) {
-              soundObject.unloadAsync();
-              setIsPlayingDownloadedAudio(false);
-            }
-          });
-        })
-        .catch((error) => {
-          console.error('Failed to load audio:', error);
+      try {
+        await downloadedSound.playAsync();
+        downloadedSound.setOnPlaybackStatusUpdate((status) => {
+          if (!status.isPlaying) {
+            setIsPlayingDownloadedAudio(false);
+          }
         });
-
-      setPlayingSound(soundObject);
-      setIsPlayingDownloadedAudio(true);
+        setIsPlayingDownloadedAudio(true);
+      } catch (error) {
+        console.error('Failed to play downloaded audio:', error);
+      }
     }
   };
 
@@ -177,74 +211,75 @@ export default function HomeScreen() {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../../assets/welcome.png')}
-          style={{ width: wp(20), height: wp(20), marginRight: 20 }}
-          contentFit="cover"
-          transition={1000}
-        />
-        <View>
-          <Text style={styles.languageText}>Language: {selectedLanguage}</Text>
-          <Text style={styles.gradeText}>Grade: {selectedGrade}</Text>
-        </View>
-      </View>
-
-      <ScrollView style={styles.messageList}>
-        <Text style={styles.messageListTitle}>Responses</Text>
-        <Text>{message}</Text>
-        {recordings.map((recordingLine, index) => (
-          <View key={index} style={styles.row}>
-            <Text style={styles.fill}>
-              Recording {index + 1} - {recordingLine.duration}
-            </Text>
-            <TouchableOpacity
-              style={styles.messageListButton}
-              onPress={() => {
-                if (playingSoundIndex === index) {
-                  playingSound.stopAsync();
-                  setPlayingSound(null);
-                  setPlayingSoundIndex(null);
-                } else {
-                  recordingLine.sound.replayAsync();
-                  setPlayingSound(recordingLine.sound);
-                  setPlayingSoundIndex(index);
-                }
-              }}
-            >
-              <Text style={styles.messageListButtonText}>
-                {playingSoundIndex === index ? 'Stop' : 'Play'}
-              </Text>
-            </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+              source={require('../../assets/welcome.png')}
+              style={{ width: wp(20), height: wp(20), marginRight: 20 }}
+              contentFit="cover"
+              transition={1000}
+          />
+          <View>
+            <Text style={styles.languageText}>Language: {selectedLanguage}</Text>
+            <Text style={styles.gradeText}>Grade: {selectedGrade}</Text>
           </View>
-        ))}
-      </ScrollView>
-
-      {downloadedAudioUri && (
-        <View style={styles.row}>
-          <Text style={styles.fill}>Downloaded Audio</Text>
-          <TouchableOpacity
-            style={styles.messageListButton}
-            onPress={toggleDownloadedAudioPlayback}
-          >
-            <Text style={styles.messageListButtonText}>
-              {isPlayingDownloadedAudio ? 'Stop' : 'Play'}
-            </Text>
-          </TouchableOpacity>
         </View>
-      )}
 
-      <TouchableOpacity
-        style={styles.recordingButton}
-        onPress={isRecording ? stopRecording : startRecording}
-      >
-        <Text style={styles.buttonText}>
-          {isRecording ? 'Stop Recording' : 'Start Recording'}
-        </Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+        <ScrollView style={styles.messageList}>
+          <Text style={styles.messageListTitle}>Responses</Text>
+          <Text>{message}</Text>
+          {recordings.map((recordingLine, index) => (
+              <View key={index} style={styles.row}>
+                <Text style={styles.fill}>
+                  Recording {index + 1} - {recordingLine.duration}
+                </Text>
+                <TouchableOpacity
+                    style={styles.messageListButton}
+                    onPress={() => {
+                      if (playingSoundIndex === index) {
+                        playingSound.stopAsync();
+                        setPlayingSound(null);
+                        setPlayingSoundIndex(null);
+                      } else {
+                        recordingLine.sound.replayAsync();
+                        setPlayingSound(recordingLine.sound);
+                        setPlayingSoundIndex(index);
+                      }
+                    }}
+                >
+                  <Text style={styles.messageListButtonText}>
+                    {playingSoundIndex === index ? 'Stop' : 'Play'}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+          ))}
+        </ScrollView>
+
+        {downloadedAudioUri && (
+            <View style={styles.row}>
+              <Text style={styles.fill}>Downloaded Audio</Text>
+              <TouchableOpacity
+                  style={styles.messageListButton}
+                  onPress={toggleDownloadedAudioPlayback}
+              >
+                <Text style={styles.messageListButtonText}>
+                  {isPlayingDownloadedAudio ? 'Stop' : 'Play'}
+                </Text>
+              </TouchableOpacity>
+            </View>
+        )}
+
+        <TouchableOpacity
+            style={[styles.recordingButton, isLoading && styles.disabledButton]}
+            onPress={isRecording ? stopRecording : startRecording}
+            disabled={isLoading}
+        >
+          <Text style={styles.buttonText}>
+            {isLoading ? 'Loading...' : isRecording ? 'Stop Recording' : 'Start Recording'}
+          </Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </SafeAreaView>
   );
 }
 
@@ -269,6 +304,16 @@ const styles = StyleSheet.create({
     fontSize: wp(4),
     color: '#fff',
     textAlign: 'center',
+  },
+  disabledButton: {
+    backgroundColor: '#ccc', // You can choose a suitable disabled color
+    zIndex: 50,
+    padding: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    opacity: 0.6, // You can adjust the opacity to make it visually disabled
   },
   imageContainer: {
     display: 'flex',
